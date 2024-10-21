76ca1cd766bfd8dd9182bbee36a992a9126259db
Khanh <phamngockhanh2723@gmail.com>
2024-10-21 17:01:41 +0700

    fix action , add zoom picAfter

--- Non_linear_Filters_Median/Form1.Designer.cs
+++ Non_linear_Filters_Median/Form1.Designer.cs
@@ -5,174 +5,183 @@ using System.Xml.Linq;
 
 namespace Non_linear_Filters_Median
 {
- partial class Form1
- {
-  private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
 
-  protected override void Dispose(bool disposing)
-  {
-   if (disposing && (components != null))
-   {
-    components.Dispose();
-   }
-   base.Dispose(disposing);
-  }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-  #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-  private void InitializeComponent()
-  {
-   picBefore = new PictureBox();
-   picAfter = new PictureBox();
-   btnChoose = new Button();
-   btnReset = new Button();
-   groupBox1 = new GroupBox();
-   groupBox2 = new GroupBox();
-   comboBoxFilter = new ComboBox();
-   trackBarBilateral = new TrackBar();
-   lblTrackBarValue = new Label();
-   btnFilter = new Button();
-   ((System.ComponentModel.ISupportInitialize)picBefore).BeginInit();
-   ((System.ComponentModel.ISupportInitialize)picAfter).BeginInit();
-   groupBox1.SuspendLayout();
-   groupBox2.SuspendLayout();
-   ((System.ComponentModel.ISupportInitialize)trackBarBilateral).BeginInit();
-   SuspendLayout();
-   // 
-   // picBefore
-   // 
-   picBefore.BackColor = SystemColors.Control;
-   picBefore.Location = new Point(21, 39);
-   picBefore.Margin = new Padding(4, 5, 4, 5);
-   picBefore.Name = "picBefore";
-   picBefore.Size = new Size(465, 551);
-   picBefore.SizeMode = PictureBoxSizeMode.StretchImage;
-   picBefore.TabIndex = 0;
-   picBefore.TabStop = false;
-   // 
-   // picAfter
-   // 
-   picAfter.Location = new Point(20, 39);
-   picAfter.Margin = new Padding(4, 5, 4, 5);
-   picAfter.Name = "picAfter";
-   picAfter.Size = new Size(465, 551);
-   picAfter.SizeMode = PictureBoxSizeMode.StretchImage;
-   picAfter.TabIndex = 1;
-   picAfter.TabStop = false;
-   // 
-   // btnChoose
-   // 
-   btnChoose.Location = new Point(622, 124);
-   btnChoose.Margin = new Padding(4, 5, 4, 5);
-   btnChoose.Name = "btnChoose";
-   btnChoose.Size = new Size(125, 59);
-   btnChoose.TabIndex = 2;
-   btnChoose.Text = "Choose Image";
-   btnChoose.UseVisualStyleBackColor = true;
-   btnChoose.Click += btnChoose_Click;
-   // 
-   // btnReset
-   // 
-   btnReset.Location = new Point(622, 489);
-   btnReset.Margin = new Padding(4, 5, 4, 5);
-   btnReset.Name = "btnReset";
-   btnReset.Size = new Size(125, 59);
-   btnReset.TabIndex = 2;
-   btnReset.Text = "Reset";
-   btnReset.UseVisualStyleBackColor = true;
-   btnReset.Click += btnReset_Click;
-   // 
-   // groupBox1
-   // 
-   groupBox1.Controls.Add(picBefore);
-   groupBox1.Location = new Point(60, 71);
-   groupBox1.Name = "groupBox1";
-   groupBox1.Size = new Size(504, 614);
-   groupBox1.TabIndex = 4;
-   groupBox1.TabStop = false;
-   groupBox1.Text = "Before";
-   // 
-   // groupBox2
-   // 
-   groupBox2.Controls.Add(picAfter);
-   groupBox2.Location = new Point(814, 71);
-   groupBox2.Name = "groupBox2";
-   groupBox2.Size = new Size(504, 614);
-   groupBox2.TabIndex = 5;
-   groupBox2.TabStop = false;
-   groupBox2.Text = "After";
-   // 
-   // comboBoxFilter
-   // 
-   comboBoxFilter.DropDownStyle = ComboBoxStyle.DropDownList;
-   comboBoxFilter.Location = new Point(622, 215);
-   comboBoxFilter.Name = "comboBoxFilter";
-   comboBoxFilter.Size = new Size(126, 28);
-   comboBoxFilter.TabIndex = 6;
-   comboBoxFilter.SelectedIndexChanged += comboBoxFilter_SelectedIndexChanged;
-   // 
-   // trackBarBilateral
-   // 
-   trackBarBilateral.Location = new Point(622, 304);
-   trackBarBilateral.Name = "trackBarBilateral";
-   trackBarBilateral.Size = new Size(125, 56);
-   trackBarBilateral.TabIndex = 7;
-   trackBarBilateral.Visible = false;
-   trackBarBilateral.Scroll += trackBarBilateral_Scroll;
-   // 
-   // lblTrackBarValue
-   // 
-   lblTrackBarValue.Location = new Point(622, 370);
-   lblTrackBarValue.Name = "lblTrackBarValue";
-   lblTrackBarValue.Size = new Size(125, 25);
-   lblTrackBarValue.TabIndex = 8;
-   lblTrackBarValue.Text = "Mịn: 50";
-   lblTrackBarValue.Visible = false;
-   // 
-   // btnFilter
-   // 
-   btnFilter.Location = new Point(623, 398);
-   btnFilter.Name = "btnFilter";
-   btnFilter.Size = new Size(125, 59);
-   btnFilter.TabIndex = 8;
-   btnFilter.Text = "Filter";
-   btnFilter.Visible = false;
-   btnFilter.Click += btnFilter_Click;
-   // 
-   // Form1
-   // 
-   AutoScaleDimensions = new SizeF(8F, 20F);
-   AutoScaleMode = AutoScaleMode.Font;
-   ClientSize = new Size(1422, 800);
-   Controls.Add(comboBoxFilter);
-   Controls.Add(trackBarBilateral);
-   Controls.Add(lblTrackBarValue);
-   Controls.Add(btnFilter);
-   Controls.Add(btnChoose);
-   Controls.Add(btnReset);
-   Controls.Add(groupBox1);
-   Controls.Add(groupBox2);
-   Name = "Form1";
-   Text = "Image Filter Application";
-   Load += Form1_Load;
-   ((System.ComponentModel.ISupportInitialize)picBefore).EndInit();
-   ((System.ComponentModel.ISupportInitialize)picAfter).EndInit();
-   groupBox1.ResumeLayout(false);
-   groupBox2.ResumeLayout(false);
-   ((System.ComponentModel.ISupportInitialize)trackBarBilateral).EndInit();
-   ResumeLayout(false);
-   PerformLayout();
-  }
-  #endregion
-  private PictureBox picBefore;
-  private PictureBox picAfter;
-  private Button btnChoose;
-  private Button btnReset;
-  private GroupBox groupBox1;
-  private GroupBox groupBox2;
-  private ComboBox comboBoxFilter;
-  private TrackBar trackBarBilateral;
-  private Label lblTrackBarValue;
-  private Button btnFilter;
- }
+        private void InitializeComponent()
+        {
+            this.picBefore = new System.Windows.Forms.PictureBox();
+            this.picAfter = new System.Windows.Forms.PictureBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBoxFilter = new System.Windows.Forms.ComboBox();
+            this.trackBarBilateral = new System.Windows.Forms.TrackBar();
+            this.lblTrackBarValue = new System.Windows.Forms.Label();
+            this.btnFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBefore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAfter)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBilateral)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBefore
+            // 
+            this.picBefore.BackColor = System.Drawing.SystemColors.Control;
+            this.picBefore.Location = new System.Drawing.Point(21, 31);
+            this.picBefore.Margin = new System.Windows.Forms.Padding(4);
+            this.picBefore.Name = "picBefore";
+            this.picBefore.Size = new System.Drawing.Size(465, 441);
+            this.picBefore.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBefore.TabIndex = 0;
+            this.picBefore.TabStop = false;
+            // 
+            // picAfter
+            // 
+            this.picAfter.Location = new System.Drawing.Point(20, 31);
+            this.picAfter.Margin = new System.Windows.Forms.Padding(4);
+            this.picAfter.Name = "picAfter";
+            this.picAfter.Size = new System.Drawing.Size(465, 441);
+            this.picAfter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAfter.TabIndex = 1;
+            this.picAfter.TabStop = false;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(622, 99);
+            this.btnChoose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(125, 47);
+            this.btnChoose.TabIndex = 2;
+            this.btnChoose.Text = "Choose Image";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(622, 391);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(125, 47);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.picBefore);
+            this.groupBox1.Location = new System.Drawing.Point(60, 57);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(504, 491);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Before";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.picAfter);
+            this.groupBox2.Location = new System.Drawing.Point(814, 57);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(504, 491);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "After";
+            // 
+            // comboBoxFilter
+            // 
+            this.comboBoxFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFilter.Location = new System.Drawing.Point(622, 172);
+            this.comboBoxFilter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBoxFilter.Name = "comboBoxFilter";
+            this.comboBoxFilter.Size = new System.Drawing.Size(126, 24);
+            this.comboBoxFilter.TabIndex = 6;
+            this.comboBoxFilter.SelectedIndexChanged += new System.EventHandler(this.comboBoxFilter_SelectedIndexChanged);
+            // 
+            // trackBarBilateral
+            // 
+            this.trackBarBilateral.Location = new System.Drawing.Point(622, 243);
+            this.trackBarBilateral.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.trackBarBilateral.Name = "trackBarBilateral";
+            this.trackBarBilateral.Size = new System.Drawing.Size(125, 56);
+            this.trackBarBilateral.TabIndex = 7;
+            this.trackBarBilateral.Visible = false;
+            this.trackBarBilateral.Scroll += new System.EventHandler(this.trackBarBilateral_Scroll);
+            // 
+            // lblTrackBarValue
+            // 
+            this.lblTrackBarValue.Location = new System.Drawing.Point(622, 296);
+            this.lblTrackBarValue.Name = "lblTrackBarValue";
+            this.lblTrackBarValue.Size = new System.Drawing.Size(125, 20);
+            this.lblTrackBarValue.TabIndex = 8;
+            this.lblTrackBarValue.Text = "Mịn: 50";
+            this.lblTrackBarValue.Visible = false;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(623, 318);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(125, 47);
+            this.btnFilter.TabIndex = 8;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.Visible = false;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 640);
+            this.Controls.Add(this.comboBoxFilter);
+            this.Controls.Add(this.trackBarBilateral);
+            this.Controls.Add(this.lblTrackBarValue);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Image Filter Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBefore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAfter)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBilateral)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private PictureBox picBefore;
+        private PictureBox picAfter;
+        private Button btnChoose;
+        private Button btnReset;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private ComboBox comboBoxFilter;
+        private TrackBar trackBarBilateral;
+        private Label lblTrackBarValue;
+        private Button btnFilter;
+    }
 }
--- Non_linear_Filters_Median/Form1.cs
+++ Non_linear_Filters_Median/Form1.cs
@@ -8,192 +8,218 @@ using OpenCvSharp.Extensions;
 
 namespace Non_linear_Filters_Median
 {
- public partial class Form1 : Form
- {
-  public Form1()
-  {
-   InitializeComponent();
-  }
-
-  private void Form1_Load(object sender, EventArgs e)
-  {
-   comboBoxFilter.Items.Clear();
-   comboBoxFilter.Items.Add("Choose Filter");
-   comboBoxFilter.Items.Add("Median");
-   comboBoxFilter.Items.Add("Bilateral");
-   comboBoxFilter.Items.Add("Gaussian");
-   comboBoxFilter.Items.Add("Nonlinear Weighted Mean");
-   comboBoxFilter.Items.Add("Anisotropic Diffusion");
-   comboBoxFilter.Items.Add("None");
-   comboBoxFilter.SelectedIndex = 0;
-
-   trackBarBilateral.Visible = false;
-   lblTrackBarValue.Visible = false;
-   btnFilter.Visible = false;
-
-   trackBarBilateral.Minimum = 1;
-   trackBarBilateral.Maximum = 100;
-   trackBarBilateral.Value = 50;
-  }
-
-  private void comboBoxFilter_SelectedIndexChanged(object sender, EventArgs e)
-  {
-   string selectedFilter = comboBoxFilter.SelectedItem.ToString();
-
-   if (selectedFilter == "Bilateral")
-   {
-    trackBarBilateral.Visible = true;
-    lblTrackBarValue.Visible = true;
-    btnFilter.Visible = false;
-    ApplySelectedFilter();
-   }
-   else
-   {
-    trackBarBilateral.Visible = false;
-    lblTrackBarValue.Visible = false;
-    btnFilter.Visible = true;
-   }
-  }
-
-  private void trackBarBilateral_Scroll(object sender, EventArgs e)
-  {
-   lblTrackBarValue.Text = "Mịn: " + trackBarBilateral.Value.ToString();
-   ApplySelectedFilter();
-  }
-
-  private void btnFilter_Click(object sender, EventArgs e)
-  {
-   ApplySelectedFilter();
-  }
-
-  private void ApplySelectedFilter()
-  {
-   if (comboBoxFilter.SelectedIndex == 0)
-   {
-    MessageBox.Show("Please select a valid filter.");
-    return;
-   }
-   if (picBefore.Image == null)
-   {
-    MessageBox.Show("Please choose an image first.");
-    return;
-   }
-   Bitmap sourceBitmap = new Bitmap(picBefore.Image);
-
-   Mat sourceMat = BitmapConverter.ToMat(sourceBitmap);
-
-   if (sourceMat.Type() != MatType.CV_8UC3 && sourceMat.Type() != MatType.CV_8UC1)
-   {
-    Cv2.CvtColor(sourceMat, sourceMat, ColorConversionCodes.BGRA2BGR);
-   }
-
-   Mat filteredMat = new Mat();
-   string selectedFilter = comboBoxFilter.SelectedItem.ToString();
-
-   switch (selectedFilter)
-   {
-    case "Median":
-     Cv2.MedianBlur(sourceMat, filteredMat, 5);
-     break;
-
-    case "Bilateral":
-     int sigmaSpace = trackBarBilateral.Value;
-     int sigmaColor = 50;
-     Cv2.BilateralFilter(sourceMat, filteredMat, 15, sigmaColor, sigmaSpace);
-     break;
-
-    case "Gaussian":
-     Cv2.GaussianBlur(sourceMat, filteredMat, new OpenCvSharp.Size(5, 5), 0);
-     break;
-
-    case "Nonlinear Weighted Mean":
-     filteredMat = ApplyNonlinearWeightedMeanFilter(sourceMat);
-     break;
-
-    case "Anisotropic Diffusion":
-     //filteredMat = ApplyAnisotropicDiffusion(sourceMat, 10, 0.25); // 10 iterations, lambda = 0.25
-     break;
-
-    case "None":
-     filteredMat = sourceMat.Clone();
-     break;
-   }
-
-   Bitmap filteredBitmap = BitmapConverter.ToBitmap(filteredMat);
-   picAfter.Image = filteredBitmap;
-  }
-
-  // Bộ lọc trung bình có trọng số phi tuyến (Nonlinear Weighted Mean Filter)
-  public Mat ApplyNonlinearWeightedMeanFilter(Mat sourceMat)
-  {
-   // Tạo bản sao của ảnh đầu vào (Mat)
-   Mat resultMat = new Mat(sourceMat.Size(), sourceMat.Type());
-
-   // Duyệt qua từng pixel của ảnh, bỏ qua viền
-   for (int y = 1; y < sourceMat.Rows - 1; y++)
-   {
-    for (int x = 1; x < sourceMat.Cols - 1; x++)
+    public partial class Form1 : Form
     {
-     double sumB = 0, sumG = 0, sumR = 0;
-     double weightSum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-     Vec3b centerPixel = sourceMat.At<Vec3b>(y, x); // Pixel tại vị trí (x, y)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxFilter.Items.Clear();
+            comboBoxFilter.Items.Add("Choose Filter");
+            comboBoxFilter.Items.Add("Median");
+            comboBoxFilter.Items.Add("Bilateral");
+            comboBoxFilter.Items.Add("Gaussian");
+            comboBoxFilter.Items.Add("Nonlinear Weighted Mean");
+            comboBoxFilter.Items.Add("Anisotropic Diffusion");
+            comboBoxFilter.Items.Add("None");
+            comboBoxFilter.SelectedIndex = 0;
 
-     // Áp dụng bộ lọc 3x3
-     for (int i = -1; i <= 1; i++)
-     {
-      for (int j = -1; j <= 1; j++)
-      {
-       Vec3b neighborPixel = sourceMat.At<Vec3b>(y + i, x + j);
+            trackBarBilateral.Visible = false;
+            lblTrackBarValue.Visible = false;
+            btnFilter.Visible = false;
 
-       // Tính trọng số phi tuyến dựa trên sự khác biệt của kênh màu đỏ (R)
-       double weight = 1.0 / (1.0 + Math.Abs(neighborPixel.Item2 - centerPixel.Item2));
+            trackBarBilateral.Minimum = 1;
+            trackBarBilateral.Maximum = 100;
+            trackBarBilateral.Value = 50;
 
-       // Cộng dồn giá trị của các kênh (B, G, R) với trọng số
-       sumB += neighborPixel.Item0 * weight; // Blue
-       sumG += neighborPixel.Item1 * weight; // Green
-       sumR += neighborPixel.Item2 * weight; // Red
+            picAfter.MouseWheel += PicAfter_MouseWheel;
 
-       // Tổng trọng số
-       weightSum += weight;
-      }
-     }
+        }
 
-     // Tính giá trị trung bình có trọng số cho mỗi kênh màu (B, G, R)
-     byte avgB = (byte)Math.Min(255, Math.Max(0, sumB / weightSum));
-     byte avgG = (byte)Math.Min(255, Math.Max(0, sumG / weightSum));
-     byte avgR = (byte)Math.Min(255, Math.Max(0, sumR / weightSum));
+        private void comboBoxFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedFilter = comboBoxFilter.SelectedItem.ToString();
 
-     // Gán giá trị mới vào ảnh kết quả
-     resultMat.Set(y, x, new Vec3b(avgB, avgG, avgR));
+            if (selectedFilter == "Bilateral")
+            {
+                trackBarBilateral.Visible = true;
+                lblTrackBarValue.Visible = true;
+                btnFilter.Visible = false;
+                ApplySelectedFilter();
+            }
+            else
+            {
+                trackBarBilateral.Visible = false;
+                lblTrackBarValue.Visible = false;
+                btnFilter.Visible = true;
+            }
+        }
+
+        private void trackBarBilateral_Scroll(object sender, EventArgs e)
+        {
+            lblTrackBarValue.Text = "Mịn: " + trackBarBilateral.Value.ToString();
+            ApplySelectedFilter();
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            ApplySelectedFilter();
+        }
+
+        private void ApplySelectedFilter()
+        {
+            if (comboBoxFilter.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select a valid filter.");
+                return;
+            }
+            if (picBefore.Image == null)
+            {
+                MessageBox.Show("Please choose an image first.");
+                return;
+            }
+            Bitmap sourceBitmap = new Bitmap(picBefore.Image);
+
+            Mat sourceMat = BitmapConverter.ToMat(sourceBitmap);
+
+            if (sourceMat.Type() != MatType.CV_8UC3 && sourceMat.Type() != MatType.CV_8UC1)
+            {
+                Cv2.CvtColor(sourceMat, sourceMat, ColorConversionCodes.BGRA2BGR);
+            }
+
+            Mat filteredMat = new Mat();
+            string selectedFilter = comboBoxFilter.SelectedItem.ToString();
+
+            switch (selectedFilter)
+            {
+                case "Median":
+                    Cv2.MedianBlur(sourceMat, filteredMat, 5);
+                    break;
+
+                case "Bilateral":
+                    int sigmaSpace = trackBarBilateral.Value;
+                    int sigmaColor = 50;
+                    Cv2.BilateralFilter(sourceMat, filteredMat, 15, sigmaColor, sigmaSpace);
+                    break;
+
+                case "Gaussian":
+                    Cv2.GaussianBlur(sourceMat, filteredMat, new OpenCvSharp.Size(5, 5), 0);
+                    break;
+
+                case "Nonlinear Weighted Mean":
+                    filteredMat = ApplyNonlinearWeightedMeanFilter(sourceMat);
+                    break;
+
+                case "Anisotropic Diffusion":
+                    //filteredMat = ApplyAnisotropicDiffusion(sourceMat, 10, 0.25); // 10 iterations, lambda = 0.25
+                    break;
+
+                case "None":
+                    filteredMat = sourceMat.Clone();
+                    break;
+            }
+
+            Bitmap filteredBitmap = BitmapConverter.ToBitmap(filteredMat);
+            picAfter.Image = filteredBitmap;
+        }
+
+        // Bộ lọc trung bình có trọng số phi tuyến (Nonlinear Weighted Mean Filter)
+        public Mat ApplyNonlinearWeightedMeanFilter(Mat sourceMat)
+        {
+            // Tạo bản sao của ảnh đầu vào (Mat)
+            Mat resultMat = new Mat(sourceMat.Size(), sourceMat.Type());
+
+            // Duyệt qua từng pixel của ảnh, bỏ qua viền
+            for (int y = 1; y < sourceMat.Rows - 1; y++)
+            {
+                for (int x = 1; x < sourceMat.Cols - 1; x++)
+                {
+                    double sumB = 0, sumG = 0, sumR = 0;
+                    double weightSum = 0;
+
+                    Vec3b centerPixel = sourceMat.At<Vec3b>(y, x); // Pixel tại vị trí (x, y)
+
+                    // Áp dụng bộ lọc 3x3
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            Vec3b neighborPixel = sourceMat.At<Vec3b>(y + i, x + j);
+
+                            // Tính trọng số phi tuyến dựa trên sự khác biệt của kênh màu đỏ (R)
+                            double weight = 1.0 / (1.0 + Math.Abs(neighborPixel.Item2 - centerPixel.Item2));
+
+                            // Cộng dồn giá trị của các kênh (B, G, R) với trọng số
+                            sumB += neighborPixel.Item0 * weight; // Blue
+                            sumG += neighborPixel.Item1 * weight; // Green
+                            sumR += neighborPixel.Item2 * weight; // Red
+
+                            // Tổng trọng số
+                            weightSum += weight;
+                        }
+                    }
+
+                    // Tính giá trị trung bình có trọng số cho mỗi kênh màu (B, G, R)
+                    byte avgB = (byte)Math.Min(255, Math.Max(0, sumB / weightSum));
+                    byte avgG = (byte)Math.Min(255, Math.Max(0, sumG / weightSum));
+                    byte avgR = (byte)Math.Min(255, Math.Max(0, sumR / weightSum));
+
+                    // Gán giá trị mới vào ảnh kết quả
+                    resultMat.Set(y, x, new Vec3b(avgB, avgG, avgR));
+                }
+            }
+
+            return resultMat;
+        }
+
+
+
+        // Bộ lọc khuếch tán dị hướng (Anisotropic Diffusion)
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    picBefore.Image = Image.FromFile(openFileDialog.FileName);
+                    picAfter.Image = null;
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            picBefore.Image = null;
+            picAfter.Image = null;
+        }
+
+        //add 
+        private float zoomFactorAfter = 1.0f;
+
+        private void PicAfter_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // Kiểm tra xem cuộn chuột lên hay xuống
+            if (e.Delta > 0) // Cuộn lên
+            {
+                zoomFactorAfter += 0.1f; // Tăng tỷ lệ phóng đại
+            }
+            else if (e.Delta < 0) // Cuộn xuống
+            {
+                zoomFactorAfter = Math.Max(zoomFactorAfter - 0.1f, 0.1f); // Giảm tỷ lệ phóng đại nhưng không dưới 0.1
+            }
+
+            // Áp dụng tỷ lệ phóng đại
+            picAfter.Width = (int)(465 * zoomFactorAfter); // Giữ kích thước tối đa
+            picAfter.Height = (int)(551 * zoomFactorAfter);
+            Bitmap sourceBitmap = new Bitmap(picAfter.Image);
+
+            // Cập nhật lại kích thước hình ảnh
+            picAfter.Image?.Dispose(); // Giải phóng hình ảnh trước đó
+            picAfter.Image = new Bitmap(sourceBitmap, picAfter.Size); // Tạo hình ảnh mới với kích thước đã thay đổi
+        }
     }
-   }
-
-   return resultMat;
-  }
-
-
-
-  // Bộ lọc khuếch tán dị hướng (Anisotropic Diffusion)
-
-
-  private void btnChoose_Click(object sender, EventArgs e)
-  {
-   using (OpenFileDialog openFileDialog = new OpenFileDialog())
-   {
-    openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
-    if (openFileDialog.ShowDialog() == DialogResult.OK)
-    {
-     picBefore.Image = Image.FromFile(openFileDialog.FileName);
-     picAfter.Image = null;
-    }
-   }
-  }
-
-  private void btnReset_Click(object sender, EventArgs e)
-  {
-   picBefore.Image = null;
-   picAfter.Image = null;
-  }
- }
 }
